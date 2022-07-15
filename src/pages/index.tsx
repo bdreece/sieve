10182bea172d6b6f899ac592cbbfef6cf7551be1
Brian Reece <bdreece@mtu.edu>
2022-07-15 19:43:24 -0400

    Updated index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,43 @@
-import type { NextPage } from 'next';
+import type { GetStaticProps, NextPage } from 'next';
+import {
+  Article,
+  getArticleContent,
+  getArticles,
+} from 'scripts/article.server';
+
 import Head from 'next/head';
 import Navbar from 'components/Navbar';
+import Footer from 'components/Footer';
+import FilterMenu from 'components/FilterMenu';
+import SortMenu from 'components/SortMenu';
+import ArticleCard from 'components/ArticleCard';
+import getSentiment from 'scripts/sentiment.server';
+import getEmotion from 'scripts/emotion.server';
 
-const Home: NextPage = () => {
+interface HomeProps {
+  articles: Article[];
+}
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+  const articles = await Promise.all(
+    (
+      await getArticles()
+    ).map(async article => {
+      const content = (await getArticleContent(article.url)) ?? '';
+      article.sentiment = await getSentiment(content);
+      article.emotion = await getEmotion(content);
+      return article;
+    })
+  );
+  return {
+    props: {
+      articles,
+    },
+    revalidate: 3600,
+  };
+};
+
+const Home: NextPage<HomeProps> = ({ articles }: HomeProps) => {
   return (
     <>
       <Head>
@@ -10,6 +45,18 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Navbar />
+      <div className="flex justify-center">
+        <FilterMenu />
+        <SortMenu />
+      </div>
+      <div className="flex flex-wrap">
+        {articles.map((article, i) => (
+          <div key={i} className="flex-none">
+            <ArticleCard article={article} />
+          </div>
+        ))}
+      </div>
+      <Footer />
     </>
   );
 };
