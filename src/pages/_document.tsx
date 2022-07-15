9ac56eaac17b2e5c3f417613aca227f6fd14ca43
Brian Reece <bdreece@mtu.edu>
2022-07-15 19:43:19 -0400

    Updated _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,10 @@ const Document = () => {
   return (
     <Html lang="en" data-theme="emerald">
       <Head>
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
