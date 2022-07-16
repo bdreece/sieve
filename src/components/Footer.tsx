d4db90699e8a6cc778d12ce53ba5f79e5c66e780
Brian Reece <bdreece@mtu.edu>
2022-07-16 10:46:23 -0400

    Added top margin to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -16,7 +16,7 @@
  */
 const Footer = () => {
   return (
-    <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+    <footer className="footer footer-center mt-4 p-4 bg-base-300 text-base-content">
       <div>
         <p>Copyright © 2022 Brian Reece</p>
       </div>
